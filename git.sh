5f95ca70bdc38b5f2443f67f7d2cc9d44bab52fc
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-18 01:18:14 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -18,7 +18,7 @@ NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
+$GIT_COMMIT=`git rev-parse HEAD`
 #NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #only tag if no tag already
@@ -30,5 +30,5 @@ GIT_COMMIT=`git rev-parse HEAD`
     #git remote add origin https://abhinavvashishat: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/abhinavvashishat/lakeshore.git
     git push --follow-tags origin https://abhinavvashishat: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/lakeshore.git $NEW_TAG 
 #else
-    echo "Already a tag on this commit"
+    #echo "Already a tag on this commit"
 #fi
