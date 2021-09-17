86af07f9025f67366c9fcf68d72d5cf1e2086e63
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-18 00:13:18 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -28,7 +28,7 @@ if [ -z "$NEEDS_TAG" ]; then
     #git remote remove origin
     #git remote set-url origin https://abhinavvashishat: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@github.com/abhinavvashishat/react-native-starter.git
     #git remote add origin https://abhinavvashishat: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@github.com/abhinavvashishat/lakeshore.git
-    git push --tag origin/* $NEW_TAG
+    git push --tag origin/develop $NEW_TAG
 else
     echo "Already a tag on this commit"
 fi
