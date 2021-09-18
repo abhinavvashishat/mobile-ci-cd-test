414d9e28ea08aa0cc650fc9822d4d65ee7072da0
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-18 09:32:41 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -28,7 +28,7 @@ echo "Updating $VERSION to $NEW_TAG"
     #git remote remove origin https://github.com/abhinavvashishat/lakeshore.git
     #git remote set-url origin https://github.com/abhinavvashishat/lakeshore.git
     #git remote add origin https://abhinavvashishat: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/abhinavvashishat/lakeshore.git
-    git push origin --tag $NEW_TAG 
+    git push --tag $NEW_TAG 
 #else
     #echo "Already a tag on this commit"
 #fi
