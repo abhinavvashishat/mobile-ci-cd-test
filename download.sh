85ae48bd2cbb8e9c7495f19510bae7b58e99eb42
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-20 19:10:08 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,12 +3,12 @@
 USER=abhinav.vashishat
 APP=Developer
 TEAM_APP='abhinav.vashishat/Developer'
-appcenter build branches show -b master=$Build_Id
-if [ "$Build_status" != "Succeeded" ]; then
-for i in {$Build_id} #downloads data from builds #1 - #10
+BUILD=appcenter build branches show -b master
+if [ "$BUILD" != "Succeeded" ]; then
+for i in {$BUILD} #downloads data from builds #1 - #10
 do
    #eval appcenter build download --id "$Build id" --app $TEAM_APP --type "logs"    
-   eval appcenter build download --id "$Build_id" --app $TEAM_APP --type "build" #uncomment to download app packages
+   eval appcenter build download --id "$BUILD" --app $TEAM_APP --type "build" #uncomment to download app packages
    #eval appcenter build download --id "$Build id" --app $TEAM_APP --type "symbols" #uncomment to download symbols
 done   
 else
