56d7672efbe6898fff846181d05e32caac8d2dac
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-20 20:29:30 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,16 +4,17 @@ USER=abhinav.vashishat
 APP=Developer
 TEAM_APP='abhinav.vashishat/Developer'
 appcenter build branches show -b master
-while read line; do
-Build_Id='echo $line'
-if [ "$Buid_Id" != "Succeeded" ]; then
-for i in {id=1} #downloads data from builds #1 - #10
+while read -r line; do
+echo "Text read from file: $line"
+done < "$Build ID"
+#if [ "$Buid ID" != "Succeeded" ]; then
+for i in {$Buid ID} #downloads data from builds #1 - #10
 do
    #eval appcenter build download --id "$Build_id" --app $TEAM_APP --type "logs"    
-   eval appcenter build download --id "1" --app $TEAM_APP --type "build" #uncomment to download app packages
+   eval appcenter build download --id "$Build ID" --app $TEAM_APP --type "build" #uncomment to download app packages
    #eval appcenter build download --id "$Build_id" --app $TEAM_APP --type "symbols" #uncomment to download symbols
 done  
-else
+#else
     Build Failed
-fi
-done
+#fi
+#done
