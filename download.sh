2359a05fe28449e7b83c4d040d0450713778cd3e
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-20 20:01:33 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,7 +8,7 @@ if [ "$BUILD" != "Succeeded" ]; then
 for i in {id=$BUILD.Build_ID} #downloads data from builds #1 - #10
 do
    #eval appcenter build download --id "$Build id" --app $TEAM_APP --type "logs"    
-   eval appcenter build download --id "$BUILD" --app $TEAM_APP --type "build" #uncomment to download app packages
+   eval appcenter build download --id "$id" --app $TEAM_APP --type "build" #uncomment to download app packages
    #eval appcenter build download --id "$Build id" --app $TEAM_APP --type "symbols" #uncomment to download symbols
 done   
 else
