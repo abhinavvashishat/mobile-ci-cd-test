d32f94e0ea3c6d86d6f8aa4bff20a1c040d32320
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-21 14:04:04 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-sleep 20m
+#sleep 20m
 LatestbuildNumber=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/abhinav.vashishat/Developer/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: 086b12763dddd7061b55e9e58b6a3b38f200d899" |jq -r '.[0].buildNumber') 
 echo $LatestbuildNumber
 appcenter build download --id "$LatestbuildNumber" --app abhinav.vashishat/Developer --type "build"
