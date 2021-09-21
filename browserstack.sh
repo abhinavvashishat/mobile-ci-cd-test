81e62cd2c971ab0d13e24355179bdc9868b2f4f8
abhinavvashishat <80381628+abhinavvashishat@users.noreply.github.com>
2021-09-21 14:45:03 +0530

    Create browserstack.sh

---
+++ browserstack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -u "abhinavvashishat_2rznLJ:7ezu9G9VfARka2RnAyrV" \
+-X POST "https://api-cloud.browserstack.com/app-automate/upload" \
+-F "file=@/var/lib/jenkins/workspace/Lakeshore-Pipeline/build_master_21_1.xcarchive"
